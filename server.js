e2f50ee22bb36e6eb3bd6080834109c625139b9c
Edmund Bates <edmund.bates@carvana.com>
2021-05-13 09:38:56 -0700

    Now I output the port even when local

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'content')));
-app.listen(process.env.PORT || 3080);
+
+const listenPort = process.env.PORT || 3080
+app.listen(listenPort);
 
 app.use(express.static('dist'))
-console.log('Online on port ' + process.env.PORT);
+console.log('Online on port ' + listenPort);
